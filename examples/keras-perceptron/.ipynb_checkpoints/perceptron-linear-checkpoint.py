774967e543d718070643cc80482ce1dc4be6e0d1
Your Name <you@example.com>
2020-02-07 00:50:23 +0000

    changes

--- examples/keras-perceptron/.ipynb_checkpoints/perceptron-linear-checkpoint.py
+++ examples/keras-perceptron/.ipynb_checkpoints/perceptron-linear-checkpoint.py
@@ -16,16 +16,15 @@ y_test = tf.keras.utils.to_categorical(y_test)
 labels = [str(i) for i in range(10)]
 
 num_classes = y_train.shape[1]
-num_hidden_units = 32
 
 # create model
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Flatten(input_shape=(img_width, img_height)))
-#model.add(tf.keras.layers.Dense(num_hidden_units, activation='relu'))
-model.add(tf.keras.layers.Dense(num_classes, activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='adam',
+model.add(tf.keras.layers.Dense(num_classes))
+model.compile(loss='mse', optimizer='adam',
               metrics=['accuracy'])
 
 # Fit the model
 model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test),
           callbacks=[wandb.keras.WandbCallback(data_type="image", labels=labels, save_model=False)])
+model.save('model.h5')
